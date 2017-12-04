930870c7b2354aa673f06139be240dd379e37718
Sam Pegler <jobs@sampegler.co.uk>
2017-12-04 12:00:19 +0000

    PR: Fix build.

--- google_auth_httplib2.py
+++ google_auth_httplib2.py
@@ -21,12 +21,8 @@ import logging
 from google.auth import exceptions
 from google.auth import transport
 import httplib2
+from six.moves import http_client  # httplib became http.client in 3.
 
-# httplib became http.client in 3.x
-try:
-    from http.client import HTTPException
-except ImportError:
-    from httplib import HTTPException
 
 _LOGGER = logging.getLogger(__name__)
 # Properties present in file-like streams / buffers.
@@ -120,9 +116,9 @@ class Request(transport.Request):
             response, data = self.http.request(
                 url, method=method, body=body, headers=headers, **kwargs)
             return _Response(response, data)
-        # TODO: httplib2 should catch the lower http error, this is a bug and
+        # httplib2 should catch the lower http error, this is a bug and
         # needs to be fixed there.  Catch the error for the meanwhile.
-        except (httplib2.HttpLib2Error, HTTPException) as exc:
+        except (httplib2.HttpLib2Error, http_client.HTTPException) as exc:
             raise exceptions.TransportError(exc)
 
 
