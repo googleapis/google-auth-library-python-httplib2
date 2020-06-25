8a5725c355601545a0bc442714b9276f08d1721e
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2020-06-25 03:47:41 +0000

    Remove warning message
    The warning is as follows:
    Warning: 'classifiers' should be a list, got type 'tuple'
    
    Reference:	https://www.python.org/dev/peps/pep-0301/#distutils-trove-classification

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=DEPENDENCIES,
     license='Apache 2.0',
     keywords='google auth oauth client',
-    classifiers=(
+    classifiers=[
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -55,5 +55,5 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
-    ),
+    ],
 )
