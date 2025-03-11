304e7b3d19ec43e6c896dd773a6ec5dca8a30dec
Anthonios Partheniou <partheniou@google.com>
2025-03-11 17:43:21 +0000

    lint

--- google_auth_httplib2.py
+++ google_auth_httplib2.py
@@ -232,7 +232,6 @@ class AuthorizedHttp(object):
             response.status in self._refresh_status_codes
             and _credential_refresh_attempt < self._max_refresh_attempts
         ):
-
             _LOGGER.info(
                 "Refreshing credentials due to a %s response. Attempt %s/%s.",
                 response.status,
