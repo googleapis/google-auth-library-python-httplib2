06a3e45d7593b01a41618a5d48807ddb05e22282
ohmayr <omairn@google.com>
2025-10-30 00:13:05 +0000

    add versions back

--- noxfile.py
+++ noxfile.py
@@ -35,6 +35,8 @@ LINT_PATHS = ["docs", "google_auth_httplib2.py", "tests", "noxfile.py", "setup.p
 DEFAULT_PYTHON_VERSION = "3.9"
 
 UNIT_TEST_PYTHON_VERSIONS: List[str] = [
+    "3.7",
+    "3.8",
     "3.9",
     "3.10",
     "3.11",
